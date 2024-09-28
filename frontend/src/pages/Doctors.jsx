cf0f317eed1e49eeb827c09b44c020edb4a71029
Sohel khan <codewithsohel786@gmail.com>
2024-09-29 00:58:51 +0530

    created in the home pages property of topDoctors , SpecialityMenu,footer, banner, and some tailwind settings

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext';
 
 const Doctors = () => {
+
+
+  const { speciality } = useParams()
+ 
+  const { doctors } = useContext(AppContext);  
+  
   return (
     <div>
       
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import Banner from '../components/Banner';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
-
+import SpecialityMenu from '../components/SpecialityMenu';
+import TopDoctors from '../components/TopDoctors';
 
 const Home = () => {
   return (
     <div>
       <Header />
-      
+      <SpecialityMenu />
+      <TopDoctors />
+      <Banner />
+     
     </div>
   );
 }
