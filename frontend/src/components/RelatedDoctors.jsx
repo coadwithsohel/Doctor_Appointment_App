e4f926388fa456dc7b83f8bb41e046632d31e572
Sohel khan <codewithsohel786@gmail.com>
2024-10-02 01:08:54 +0530

    completed all doctors and appointment page

--- frontend/src/components/RelatedDoctors.jsx
+++ frontend/src/components/RelatedDoctors.jsx
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
 import { useNavigate } from 'react-router-dom';
-import { AppContext } from '../context/AppContext';
-const TopDoctors = () => {
 
-  const navigate = useNavigate();
-  const {doctors} = useContext(AppContext)
+const RelatedDoctors = ({speciality, docId}) => {
+  const { doctors } = useContext(AppContext);
+  const navigate = useNavigate()
+
+    const [relDoc, setRelDoc] = useState([]);
+
+    useEffect(() => {
+        if (doctors.length > 0 && speciality) {
+            const doctorsData = doctors.filter((doc)=> doc.speciality === speciality && doc._id != docId)
+            setRelDoc(doctorsData)
+        }
+    },[doctors,speciality,docId])
 
   return (
     <div className="flex flex-col items-center gap-4 my-16 text-gray-600 md:mx-10 ">
@@ -13,12 +22,9 @@ const TopDoctors = () => {
         Simply browse through our extensive list of trusted doctors.
       </p>
       <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
-        {doctors.slice(0, 10).map((item, index) => (
+        {relDoc.slice(0, 5).map((item, index) => (
           <div
-            onClick={() => {
-              navigate(`/appointment/${item._id}`);
-              scrollTo(0, 0);
-            }}
+                onClick={() => { navigate(`/appointment/${item._id}`); scrollTo(0,0)}}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
@@ -45,6 +51,6 @@ const TopDoctors = () => {
       </button>
     </div>
   );
-}
+};
 
-export default TopDoctors
+export default RelatedDoctors
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ import AppContextProvider from './context/AppContext.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppContextProvider>
-    <App />
+    <App/>
       
   </AppContextProvider>
   </BrowserRouter>,
